4b8007a702e23246d15b30fb8662ecc45b530105
rafaelgckto <rafael.gcecatto@gmail.com>
2019-11-18 15:56:22 -0300

    v2.1

--- src/pages/Categorias.js
+++ src/pages/Categorias.js
@@ -9,6 +9,11 @@ class Categoria extends Component {
             listaCategorias: [],
             titulo: ''
         }
+
+        // Chamar funções do projeto
+        this.AtualizaEstadoTitulo = this.AtualizaEstadoTitulo.bind(this);
+        this.BuscarCategorias = this.BuscarCategorias.bind(this);
+        this.CadastrarCategoria = this.CadastrarCategoria.bind(this);
     }
 
     /* 
@@ -16,16 +21,37 @@ class Categoria extends Component {
         Atribui os dados recebidos ao state listaCategorias
         Caso ocorra um erro, exibe no console do navegador
     */
-    buscarCategorias() {
+    BuscarCategorias() {
         fetch('http://localhost:5000/api/categorias')
             .then(resposta => resposta.json())
-            .then(data => this.setState({ listaCategorias : data}))
+            .then(data => this.setState({ listaCategorias: data }))
             .catch((erro) => console.log(erro));
     }
 
+    // Recebe um evento, e recebo o valor do campo titulo
+    AtualizaEstadoTitulo(event) {
+        this.setState({ titulo: event.target.value });// titulo vai receber um estado de evento
+    }
+
+    CadastrarCategoria(event) {
+        event.preventDefault();// Faz cessar comportamentos padrões da página 
+
+        fetch('http://localhost:5000/api/categorias', {
+            method: 'POST',// Declara o método que será utilizado
+            body: JSON.stringify({ titulo: this.state.titulo }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        }).then(resposta => {
+            if (resposta.status === 200) {
+                console.log("Categoria cadastrada com sucesso!");// Mensagem de sucesso
+            }
+        }).catch(erro => console.log(erro)).then(this.BuscarCategorias());
+    }
+
     // Assim que a página for carregada, chama a função buscarCategoria
-    componentDidMount(){
-        this.buscarCategorias();
+    componentDidMount() {
+        this.BuscarCategorias();
     }
 
     render() {
@@ -62,8 +88,8 @@ class Categoria extends Component {
                                         {
                                             // Percorre o array listaCategoria e preenche o corpo da
                                             // com o ID e o título de cada categoria
-                                            this.state.listaCategorias.map(function(categoria){
-                                                return(
+                                            this.state.listaCategorias.map(function (categoria) {
+                                                return (
                                                     <tr key={categoria.categoriaId}>
                                                         <td>{categoria.categoriaId}</td>
                                                         <td>{categoria.titulo}</td>
@@ -79,15 +105,22 @@ class Categoria extends Component {
                             <div class="container" id="conteudoPrincipal-cadastro">
                                 <h2 class="conteudoPrincipal-cadastro-titulo">
                                     Cadastrar Tipo de Evento
-                                    </h2>
-                                <form>
+                                </h2>
+                                {/* 
+                                    Adicionar eventos no formulários
+                                    Adicionar evento para submeter requisição e chamar a função a ser
+                                */}
+                                <form onSubmit={this.CadastrarCategoria}>
                                     <div class="container">
                                         <input
+                                            value={this.state.titulo}// O valor digitado no input vai para 
+                                            onChange={this.AtualizaEstadoTitulo}// Evento do formulário
                                             type="text"
                                             id="nome-tipo-evento"
                                             placeholder="tipo do evento"
                                         />
                                         <button
+                                            type="submit"
                                             class="conteudoPrincipal-btn conteudoPrincipal-btn-cadastro">
                                             Cadastrar
                                           </button>
--- src/pages/Eventos.js
+++ src/pages/Eventos.js
@@ -2,6 +2,76 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class Evento extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listaEventos: [],
+            titulo: "",
+            dataEvento: "",
+            acessoLivre: "",
+            categoria: ""
+        }
+
+        // Chamar funções do projeto
+        this.BuscarEventos = this.BuscarEventos.bind(this);
+        this.AtualizarEstadoTitulo = this.AtualizarEstadoTitulo.bind(this);
+        this.AtualizarEstadoData = this.AtualizarEstadoData.bind(this);
+        this.AtualizarEstadoAcesso = this.AtualizarEstadoAcesso.bind(this);
+        this.CadastrarEvento = this.CadastrarEvento.bind(this);
+    }
+
+    /*
+        Função que faz a requisição para a API
+        Atribui os dados recebidos ao state listaEvento
+        Caso ocorra um erro, exibe no console do navegador
+    */
+    // Listar eventos da API
+    BuscarEventos() {
+        fetch('http://localhost:5000/api/eventos')
+            .then(resposta => resposta.json())
+            .then(data => this.setState({ listaEventos: data }))
+            .catch((erro) => console.log(erro));
+    }
+
+    // Recebe um evento, e recebe o valor do campo
+    AtualizarEstadoTitulo(event) {
+        this.setState({ titulo: event.target.value });// titulo vai receber um estado de evento
+    }
+
+    AtualizarEstadoData(event) {
+        this.setState({ dataEvento: event.target.value });// dataEvento vai receber um estado de evento
+    }
+
+    AtualizarEstadoAcesso(event) {
+        this.setState({ acessoLivre: event.target.value });// acessolivre vai receber um estado de evento
+    }
+
+    CadastrarEvento(event) {
+        event.preventDefault();// Faz cessar comportamentos padrões da página
+
+        fetch('http://localhost:5000/api/eventos', {
+            method: 'POST',// Declara o método que será utilizado
+            body: JSON.stringify({
+                titulo: this.state.titulo,
+                dataEvento: this.state.dataEvento,
+                acessoLivre: this.state.acessoLivre
+            }),
+            headers: {
+                "Content-type": "application/json"
+            }
+        }).then(resposta => {
+            if (resposta.status === 200) {
+                console.log("Evento cadastrado com sucesso!");// Mensagem de sucesso
+            }
+        }).catch(erro => console.log(erro)).then(this.BuscarEventos());
+    }
+
+    // Assim que a página for carregada, chama a função BuscarEventos()
+    componentDidMount() {
+        this.BuscarEventos();
+    }
+
+    // Renderizar o corpo da página
     render() {
         return (
             <div className='App'>
@@ -31,39 +101,71 @@ class Evento extends Component {
                                     </tr>
                                 </thead>
 
-                                <tbody id="tabela-lista-corpo"></tbody>
+                                <tbody id="tabela-lista-corpo">
+                                    {/* Utilizar função e map para preencher a lista */}
+
+                                    {/* Percorre o array listaEvento e preenche o corpo da '' com o ID e o título de cada evento */}
+                                    {
+                                        this.state.listaEventos.map(function (evento) {
+                                            return (
+                                                <tr key={evento.eventoId}>
+                                                    <td>{evento.eventoId}</td>
+                                                    <td>{evento.titulo}</td>
+                                                    <td>{evento.dataEvento}</td>
+                                                    <td>{evento.acessoLivre ? "Público" : "Privado"}</td>
+                                                    {/* <td>{evento.categoria.titulo}</td> */}
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
                             </table>
                         </div>
 
-                        <div class="container" id="conteudoPrincipal-cadastro">
-                            <h2 class="conteudoPrincipal-cadastro-titulo">Cadastrar Evento</h2>
-                            <div class="container">
-                                <input
-                                    type="text"
-                                    id="evento__titulo"
-                                    placeholder="título do evento"
-                                />
-                                <input type="text" id="evento__data" placeholder="dd/MM/yyyy" />
-                                <select id="option__acessolivre">
-                                    <option value="1">Livre</option>
-                                    <option value="0">Restrito</option>
-                                </select>
-                                <select id="option__tipoevento">
-                                    <option value="0" disabled>Tipo do Evento</option>
-                                </select>
-                                <textarea
-                                    rows="3"
-                                    cols="50"
-                                    placeholder="descrição do evento"
-                                    id="evento__descricao"
-                                ></textarea>
-                            </div>
-                            <button
-                                class="conteudoPrincipal-btn conteudoPrincipal-btn-cadastro"
-                                onclick="cadastrarEvento()">
-                                Cadastrar
+                        <form onSubmit={this.CadastrarEvento}>
+                            <div className="container" id="conteudoPrincipal-cadastro">
+                                <h2 className="conteudoPrincipal-cadastro-titulo">Cadastrar Evento</h2>
+                                <div className="container">
+                                    <input
+                                        value={this.state.titulo}
+                                        onChange={this.AtualizarEstadoTitulo}
+                                        type="text"
+                                        id="evento__titulo"
+                                        placeholder="título do evento"
+                                    />
+                                    <input
+                                        value={this.state.dataEvento}
+                                        onChange={this.AtualizarEstadoData}
+                                        type="date"
+                                        id="evento__data"
+                                        placeholder="dd/MM/yyyy"
+                                    />
+                                    <select
+                                        value = {this.state.acessoLivre}
+                                        onChange = {this.AtualizarEstadoAcesso}
+                                        id="option__acessolivre">
+                                        <option value="1">Publico</option>
+                                        <option value="0">Privado</option>
+                                    </select>
+                                    <select id="option__tipoevento">
+                                        <option value="0">Tipo do Evento</option>
+                                        <option value="1">teste1</option>
+                                        {/* <option>{}</option> */}
+                                    </select>
+                                    <textarea
+                                        rows="3"
+                                        cols="50"
+                                        placeholder="descrição do evento"
+                                        id="evento__descricao"
+                                    ></textarea>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="conteudoPrincipal-btn conteudoPrincipal-btn-cadastro">
+                                    Cadastrar
                                 </button>
-                        </div>
+                            </div>
+                        </form>
                     </section>
                 </main>
 
